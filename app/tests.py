94d45c8a1f1e889cd0e51d0067734efcfead001c
Karthik <kartisuri@gmail.com>
2018-04-30 09:28:39 +0800

    added selenium test case

--- app/tests.py
+++ app/tests.py
@@ -1,17 +1,25 @@
+import re
+import os
+import threading
+import time
 import unittest
+import logging
+from selenium import webdriver
 
 from app import app
 from database import Database
 
 class TestCase(unittest.TestCase):
+    client = None
+
     def setUp(self):
         self.app = app.test_client()
         self.db = Database()
         self.db.create_table()
+        
 
     def tearDown(self):
         self.db.drop_table()
-        self.db.create_table()
         self.db.close_conn()
 
     def test_create_movie(self):
@@ -38,6 +46,31 @@ class TestCase(unittest.TestCase):
             self.db.insert_movie(movie['name'], movie['cover'])
         result = self.db.fetch_movies()
         assert result == movies
+
+    def test_selenium(self):
+        threading.Thread(target=app.run).start()
+        time.sleep(3)
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        options.add_argument('headless')
+        try:
+            self.client = webdriver.Chrome(chrome_options=options,
+                service_args=["--verbose", "--log-path=test-reports/chrome.log"])
+        except Exception as e:
+            print(e)
+            self.skipTest('Web browser not available')
+        # navigate to index page
+        self.client.get('http://localhost:5000/')
+        self.assertTrue(re.search('Hello,\s+Stranger!', self.client.page_source))
+
+        self.client.find_element_by_name('name').send_keys('Avengers')
+        self.client.find_element_by_name('cover').send_keys('https://image.tmdb.org/t/p/w1280/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg')
+        self.client.find_element_by_id('create').click()
+        time.sleep(1)
+        print(self.client.page_source)
+        self.assertTrue(r'<h4 class="card-title" ng-binding>Avengers</h4>' in self.client.page_source)
+        self.assertTrue(r'src="https://image.tmdb.org/t/p/w1280/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"' in self.client.page_source)
+        self.client.quit()
 
 if __name__ == "__main__":
     unittest.main()
